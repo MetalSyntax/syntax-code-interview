8e22ab9788349508a565f702ac0b36fe4333f65b
Wonder Diaz <wonderdxk@gmail.com>
2021-12-14 22:29:12 -0400

    Improve
    Loop to keys
    Fix the valid
    Display the words
    Improve styles

--- App.js
+++ App.js
@@ -1,63 +1,58 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableNativeFeedback } from 'react-native';
-
-export default function App() {
-  const keyBoard = require('./data/test-board-2.json');
-
-  const [color, setColor] = useState(null);
-  
-  function handleClick() {
-    setColor(true)
+import { StyleSheet, Text, View, TextInput, Button, StatusBar, SafeAreaView, TouchableOpacity } from 'react-native';
+const key = require('./data/test-board-2.json');
+const dictionary = require('./data/dictionary.json');
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tap: '',
+      isTap: false
+    };
   }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.rowClear}>
-        <Text style={styles.close}>
-          Clear
-        </Text>
+  handleTap(value) {
+    this.setState({tap: this.state.tap + value})
+  }
+
+  render() {
+    const keyboard = []
+    for (var i = 0; i < key.board.length; i += 4) {
+      keyboard.push(<View style={styles.row}>
+        {key.board.slice(i, i + 4).map((keys, index) => (
+          <TouchableOpacity style={styles.touchable} onPress={() => this.handleTap(keys)}>
+              <Text style={styles.button}>
+                {keys}
+              </Text>
+          </TouchableOpacity>
+        ))}
+      </View>)
+    }
+    return (
+      <View style={styles.container}>
+        <StatusBar/>
+        <SafeAreaView>
+        <View style={styles.rowClose}>
+          <Text style={styles.close}>
+            Clear
+          </Text>
+        <TouchableOpacity style={styles.rowClear} onPress={() => this.setState({ tap: '', isTap: false })}>
+          <Text style={styles.ex}>
+            X
+          </Text>
+        </TouchableOpacity>
+        </View>
+        {keyboard}
+        <View style={styles.row}>
+          <TextInput style={styles.input} editable={false} selectTextOnFocus={false} value={this.state.tap.toLowerCase()} placeholder="Insert word" placeholderTextColor="ABB2B9"/>
+        </View>
+        { this.state.tap.length > 2 ? dictionary.words.includes(this.state.tap.toLowerCase()) 
+        ? <View style={styles.rowValid}><Text style={styles.true}>Valid</Text></View> 
+        : <View style={styles.rowValid}><Text style={styles.false}>Invalid</Text></View> : null}
+        </SafeAreaView>
       </View>
-      <View style={styles.row}>
-      {keyBoard.board.slice(0, 4).map((keys) => (
-        <TouchableNativeFeedback style={styles.touchable}>
-            <Text style={styles.button}>
-              {keys}
-            </Text>
-        </TouchableNativeFeedback>
-      ))}
-      </View>
-      <View style={styles.row}>
-      {keyBoard.board.slice(4, 8).map((keys) => (
-        <TouchableNativeFeedback style={styles.touchable}>
-            <Text style={styles.button}>
-              {keys}
-            </Text>
-        </TouchableNativeFeedback>
-      ))}
-      </View>
-      <View style={styles.row}>
-      {keyBoard.board.slice(8, 12).map((keys) => (
-        <TouchableNativeFeedback style={styles.touchable}>
-            <Text style={styles.button}>
-              {keys}
-            </Text>
-        </TouchableNativeFeedback>
-      ))}
-      </View>
-      <View style={styles.row}>
-      {keyBoard.board.slice(12, 16).map((keys) => (
-        <TouchableNativeFeedback style={styles.touchable}>
-            <Text style={styles.button}>
-              {keys}
-            </Text>
-        </TouchableNativeFeedback>
-      ))}
-      </View>
-      <View style={styles.row}>
-        <TextInput style={styles.input} editable={false} selectTextOnFocus={false}/>
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -82,7 +77,7 @@ const styles = StyleSheet.create({
   button: {
     color: '#FFF',
     backgroundColor: '#F5B041',
-    padding: 10,
+    padding: 7,
     width: 50,
     height: 50,
     fontSize: 30,
@@ -91,14 +86,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     cursor: 'pointer',
-    borderRadius: 10,
+    borderRadius: 50,
     borderWidth: 1,
     borderColor: '#D5DBDB'
   },
-  true: {
+  trueButton: {
     color: '#FFF',
     backgroundColor: '#2ECC71',
-    padding: 10,
+    padding: 7,
     width: 50,
     height: 50,
     fontSize: 30,
@@ -107,14 +102,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     cursor: 'pointer',
-    borderRadius: 10,
+    borderRadius: 50,
     borderWidth: 1,
     borderColor: '#D5DBDB'
   },
-  false: {
+  falseButton: {
     color: '#FFF',
     backgroundColor: '#CB4335',
-    padding: 10,
+    padding: 7,
     width: 50,
     height: 50,
     fontSize: 30,
@@ -123,7 +118,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     cursor: 'pointer',
-    borderRadius: 10,
+    borderRadius: 50,
     borderWidth: 1,
     borderColor: '#D5DBDB'
   },
@@ -136,9 +131,41 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     width: '100%'
   },
+  rowValid: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  true: {
+    color: '#2ECC71',
+    fontSize: 18,
+  },
+  false: {
+    color: '#CB4335',
+    fontSize: 18,
+  },
   close: {
     color: '#ABB2B9',
+    fontSize: 18,
+  },
+  ex: {
+    color: '#FFF',
+    backgroundColor: '#ABB2B9',
+    padding: 7,
+    width: 40,
+    height: 40,
+    fontSize: 20,
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     cursor: 'pointer',
-    fontSize: 18
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: '#ABB2B9'
+  },
+  rowClose: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   }
 });
